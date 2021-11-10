4b45275bd9bf4fed225e549a3da8abe115b5df02
ProMoFu <promofu@online.de>
2021-11-10 23:32:35 +0100

    playing some more

---
+++ src/test/java/com/pluralsight/springdataoverview/SpringDataOverviewApplicationTests.java
@@ -0,0 +1,15 @@
+package com.pluralsight.springdataoverview;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringDataOverviewApplicationTests {
+
+    @Test
+    public void contextLoads() {}
+
+}
